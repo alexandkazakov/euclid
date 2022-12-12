421c85e87d7b178de4e94ec81c643fbc28b90697
Alexander Kazakov <alexandkazakov@ya.ru>
2022-12-12 13:31:43 +0300

    js tabs in work section added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,25 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
+
+let workStepsLinks = document.querySelectorAll('.work__nav .nav__link');
+// workStepsLinks.forEach(function(link) {
+//   alert(link);
+// });
+let workStepsBlocks = document.querySelectorAll('.work__wrap');
+
+workStepsLinks.forEach(function(element) {
+  element.addEventListener('click', function(elem) {
+    let workStepsPath = elem.currentTarget.dataset.path;
+
+    workStepsLinks.forEach(function(link) {
+      link.classList.remove('nav__link_active');
+    });
+    elem.currentTarget.classList.add('nav__link_active');
+
+    workStepsBlocks.forEach(function(block) {
+      block.classList.remove('work__wrap_visible');
+    });
+    document.querySelector(`[data-target="${workStepsPath}"]`).classList.add('work__wrap_visible');
+  });
+});
